cb151589744cf37d6915b63533281dcf568cea15
Uroš Radunović <radunovicuros0@gmail.com>
2023-06-21 23:08:00 +0200

    Resolve merge conflict in App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-<<<<<<< HEAD
-import './App.css';
-import NewUserForm from './components/NewUserForm';
-
-function App() {
-  return (
-    <div className="App">
-      <NewUserForm/>
-    </div>
-=======
 import { Fragment } from "react";
 
+import NewUserForm from "./components/NewUserForm";
 import MainHeader from "./components/UI/MainHeader";
 import Sidebar from "./components/UI/Sidebar";
 
 function App() {
   return (
-    <Fragment>
-      <MainHeader />
-      <main>
-        <Sidebar />
-      </main>
-    </Fragment>
->>>>>>> da49e2883e995d070642e52feaf4deb5854c06a0
+    <div className="App">
+      <Fragment>
+        <MainHeader />
+        <main>
+          {/* <Sidebar /> */}
+          <NewUserForm />
+        </main>
+      </Fragment>
+    </div>
   );
 }
-
 export default App;
